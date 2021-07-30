4b821c975afa0863eefe953690c33e3025b713c3
Qstick <qstick@gmail.com>
2021-07-29 22:31:10 -0400

    New: Switch to ASPNetCore Kestrel and SignalR
    Co-Authored-By: ta264 <ta264@users.noreply.github.com>

--- frontend/build/webpack.config.js
+++ frontend/build/webpack.config.js
@@ -45,6 +45,14 @@ module.exports = (env) => {
       ],
       alias: {
         jquery: 'jquery/src/jquery'
+      },
+      fallback: {
+        buffer: false,
+        http: false,
+        https: false,
+        url: false,
+        util: false,
+        net: false
       }
     },
 
